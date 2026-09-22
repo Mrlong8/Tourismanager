160b866be22757d1e2ff589d02a4f55a56c41e1e
Mrlong8 <xuanlong1806@gmail.com>
2026-09-22 23:03:42 +0700

    add map

---
+++ TourisManager/Controllers/MapController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TourisManager.Services;
+
+namespace TourisManager.Controllers
+{
+    public class MapController : Controller
+    {
+        private readonly LocationService _locationService;
+
+        public MapController(LocationService locationService)
+        {
+            _locationService = locationService;
+        }
+
+        public IActionResult Index()
+        {
+            var listLocations = _locationService.GetDataAll();
+            return View(listLocations);
+        }
+    }
+}
